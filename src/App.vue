258ff4f90dcf7105591d37f93fb12b0e14127092
Andrew Cheng <andrew.cheng@visaops.com>
2022-10-19 10:51:35 -0400

    Cleaning up

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,28 @@
 <template>
-  <input v-model="message" placeholder="Symbol" />
+  <input v-model="symbol" placeholder="Symbol" />
   <br>
   <button @click="getQuote">Get Quote</button>
   <br>
-  <h2 v-if="post">Symbol: {{ post.symbol }}</h2>
+  <h2 v-if="stock">Symbol: {{ stock.symbol }}</h2>
   <br>
-  <h2 v-if="post">Price: {{ post.latest_price }}</h2>
+  <h2 v-if="stock">Price: {{ stock.latest_price }}</h2>
   <br>
-  <h2 v-if="post">Change: {{ post.change }}</h2>
+  <h2 v-if="stock">Change: {{ stock.change }}</h2>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      post: [],
-      message: 'GOOG'
+      stock: [],
+      symbol: 'GOOG'
     }
   },
   methods: {
     getQuote() {
-      fetch('http://localhost:3333/api/v1/stocks?symbol=' + this.message)
+      fetch('http://localhost:3333/api/v1/stocks?symbol=' + this.symbol)
         .then(response => response.json())
-        .then(response => this.post = response.data)
-    },
-    postSymbol() {
-      fetch('http://localhost:3333/api/v1/friends')
-          .then(response => response.json())
-          .then(response => this.post = response.data)
+        .then(response => this.stock = response.data)
     }
   }
 }
