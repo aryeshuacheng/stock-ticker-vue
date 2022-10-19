a49af335d9bf26ca4948c58355c7daa46a69cf6c
Andrew Cheng <andrew.cheng@visaops.com>
2022-10-19 14:46:47 -0400

    Added a UI to add and get stock quotes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
+  <center>
+  <h1><b>Karma Stock Ticker!</b></h1>
   <input v-model="symbol" placeholder="Symbol" />
+  <button @click="addStockToPortfolio">Add Stock</button>
   <br>
-  <button @click="getQuote">Get Quote</button>
   <br>
-  <h2 v-if="stock">Symbol: {{ stock.symbol }}</h2>
+  <button @click="getQuotes">Get Quotes</button>
   <br>
-  <h2 v-if="stock">Price: {{ stock.latest_price }}</h2>
   <br>
-  <h2 v-if="stock">Change: {{ stock.change }}</h2>
+  <b>Portfolio</b>
+  <br>
+  <br>
+  <table class="table table-bordered table-striped">
+    <thead>
+    <tr>
+      <th scope="col">Symbol</th>
+      <th scope="col">Price</th>
+      <th scope="col">Change</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="stock in stocks">
+      <td>{{stock.symbol}}</td>
+      <td>{{stock.latest_price}}</td>
+      <td>{{stock.change}}</td>
+    </tr>
+    </tbody>
+  </table>
+  </center>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      stock: [],
+      stocks: [],
       symbol: 'GOOG'
     }
   },
   methods: {
-    getQuote() {
-      fetch('http://localhost:3333/api/v1/stocks?symbol=' + this.symbol)
+    getQuotes() {
+      fetch('http://localhost:3333/api/v1/get_quotes')
         .then(response => response.json())
-        .then(response => this.stock = response.data)
+        .then(response => this.stocks = response.data)
+        .then(response => console.log(response))
+    },
+    addStockToPortfolio() {
+      fetch('http://localhost:3333/api/v1/add_stock_to_portfolio?symbol=' + this.symbol)
     }
   }
 }
