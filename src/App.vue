ed234bc9d936fdb6b12ead4dbfe442421efd8456
Andrew Cheng <andrew.cheng@visaops.com>
2022-10-20 12:20:56 -0400

    Added templates for new methods

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <center>
   <h1><b>Karma Stock Ticker!</b></h1>
+  <input v-model="name" placeholder="Name" />
+  <br>
   <input v-model="symbol" placeholder="Symbol" />
+  <br>
   <button @click="addStockToPortfolio">Add Stock</button>
   <br>
   <br>
@@ -24,6 +27,10 @@
       <td>{{stock.symbol}}</td>
       <td>{{stock.latest_price}}</td>
       <td>{{stock.change}}</td>
+      <td>{{stock.shares}}</td>
+      <td> <button @click="buyStock">Buy</button></td>
+      <td> <button @click="sellStock">Sell</button></td>
+      <td> <button @click="removeFromPortfolio">Remove from Portfolio</button></td>
     </tr>
     </tbody>
   </table>
@@ -34,20 +41,29 @@
 export default {
   data() {
     return {
+      name: '',
       stocks: [],
       symbol: 'GOOG'
     }
   },
   methods: {
     getQuotes() {
-      fetch('http://localhost:3333/api/v1/get_quotes')
+      fetch('http://localhost:3333/api/v1/get_quotes'+'?name=' + this.name)
         .then(response => response.json())
         .then(response => this.stocks = response.data)
-        .then(response => console.log(response))
     },
     addStockToPortfolio() {
-      fetch('http://localhost:3333/api/v1/add_stock_to_portfolio?symbol=' + this.symbol)
-    }
+      fetch('http://localhost:3333/api/v1/add_stock_to_portfolio?symbol=' + this.symbol + '&name=' + this.name)
+    },
+    removeFromPortfolio() {
+      fetch('http://localhost:3333/api/v1/remove_stock_from_portfolio?symbol=' + this.symbol + '&name=' + this.name)
+    },
+    buyStock() {
+      fetch('http://localhost:3333/api/v1/buy_stock?symbol=' + this.symbol + '&name=' + this.name)
+    },
+    sellStock() {
+      fetch('http://localhost:3333/api/v1/sell_stock?symbol=' + this.symbol + '&name=' + this.name)
+    },
   }
 }
 </script>
