3f29ad4f4915ddc7e2040cdde9f38493f4c28120
Andrew Cheng <andrew.cheng@visaops.com>
2022-10-20 13:29:20 -0400

    Added Bootstrap Vue 3 styling

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,47 @@
 <template>
   <center>
   <h1><b>Karma Stock Ticker!</b></h1>
-  <input v-model="name" placeholder="Name" />
+  <input v-model="name" placeholder="Player Name" />
   <br>
-  <input v-model="symbol" placeholder="Symbol" />
-  <br>
-  <button @click="addStockToPortfolio">Add Stock</button>
+
   <br>
   <br>
-  <button @click="getQuotes">Get Quotes</button>
+  <input v-model="symbol" placeholder="Symbol" />&nbsp&nbsp&nbsp<b-button variant="primary" @click="addStockToPortfolio">Add Stock</b-button>
+  <br>
+  <br>
+  <b-button variant="primary" @click="getQuotes">Get Quotes</b-button>
   <br>
   <br>
   <b>Portfolio</b>
   <br>
+  Cash Balance: ${{available_cash}}
   <br>
-  <table class="table table-bordered table-striped">
-    <thead>
-    <tr>
-      <th scope="col">Symbol</th>
-      <th scope="col">Price</th>
-      <th scope="col">Change</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr v-for="stock in stocks">
-      <td>{{stock.symbol}}</td>
-      <td>{{stock.latest_price}}</td>
-      <td>{{stock.change}}</td>
-      <td>{{stock.shares}}</td>
-      <td> <button @click="buyStock">Buy</button></td>
-      <td> <button @click="sellStock">Sell</button></td>
-      <td> <button @click="removeFromPortfolio">Remove from Portfolio</button></td>
-    </tr>
-    </tbody>
-  </table>
+  <b-table-simple bordered small responsive>
+    <b-thead>
+    <b-tr variant="dark">
+      <b-th scope="col">Symbol</b-th>
+      <b-th scope="col">Price</b-th>
+      <b-th scope="col">Change</b-th>
+      <b-th scope="col">Shares</b-th>
+      <b-th scope="col"></b-th>
+      <b-th scope="col"></b-th>
+      <b-th scope="col"></b-th>
+    </b-tr>
+    </b-thead>
+    <b-tbody>
+    <b-tr v-for="stock in stocks">
+      <b-td>{{stock.symbol}}</b-td>
+      <b-td>{{stock.latest_price}}</b-td>
+      <b-td>{{stock.change}}</b-td>
+      <b-td>{{stock.shares}}</b-td>
+      <b-card>
+      <b-td><b-button variant="success" @click="buyStock">Buy</b-button></b-td>
+      <b-td><b-button variant="danger" @click="sellStock">Sell</b-button></b-td>
+      <b-td><b-button @click="removeFromPortfolio">Remove from Portfolio</b-button></b-td>
+      </b-card>
+    </b-tr>
+    </b-tbody>
+  </b-table-simple>
   </center>
 </template>
 
@@ -43,14 +51,19 @@ export default {
     return {
       name: '',
       stocks: [],
-      symbol: 'GOOG'
+      symbol: 'GOOG',
+      available_cash: 0
     }
   },
   methods: {
     getQuotes() {
-      fetch('http://localhost:3333/api/v1/get_quotes'+'?name=' + this.name)
-        .then(response => response.json())
-        .then(response => this.stocks = response.data)
+      fetch('http://localhost:3333/api/v1/get_quotes' + '?name=' + this.name)
+          .then(response => response.json())
+          .then(response => this.stocks = response.data)
+
+      fetch('http://localhost:3333/api/v1/available_cash?name=' + this.name)
+          .then(response => response.json())
+          .then(response => this.available_cash = response)
     },
     addStockToPortfolio() {
       fetch('http://localhost:3333/api/v1/add_stock_to_portfolio?symbol=' + this.symbol + '&name=' + this.name)
@@ -63,7 +76,7 @@ export default {
     },
     sellStock() {
       fetch('http://localhost:3333/api/v1/sell_stock?symbol=' + this.symbol + '&name=' + this.name)
-    },
+    }
   }
 }
 </script>
